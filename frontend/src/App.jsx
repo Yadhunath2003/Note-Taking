964aba272e14c994cae3fdad0d4ef2abcdd866c6
Yadhunath2003 <yadhunath2003@gmail.com>
2025-09-07 17:36:50 -0500

    Added pages to App.jsx and completed NotFound page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
+import Home from './pages/Home'
+import ProtectedRoutes from './components/ProtectedRoutes'
 
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout(){
+  localStorage.clear();
+  return <Register />
+}
 
 function App() {
   
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <Home />
+            </ProtectedRoutes>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
