77dbe7707e580e82c8e84df91e7632928510f181
Yadhunath2003 <yadhunath2003@gmail.com>
2025-09-29 21:13:50 -0500

    updated the connections

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants'; // Adjust the path as necessary
 
+const apiUrl = /choreo-apis/note-taking/backend/v1; 
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, // Ensure this is set in your .env file
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl, // Ensure this is set in your .env file
     headers: {
         'Content-Type': 'application/json',
     },
