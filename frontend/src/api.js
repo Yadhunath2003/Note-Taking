169770d7e6eaa4f50e8b00f4e4a920694cdab50e
Yadhunath2003 <yadhunath2003@gmail.com>
2025-09-30 00:01:21 -0500

    updated api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants'; // Adjust the path as necessary
 
-const apiUrl = "/choreo-apis/backend-743961051:8000"; 
+const apiUrl = "/choreo-apis/note-taking/backend/v1"; 
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl, // Ensure this is set in your .env file
